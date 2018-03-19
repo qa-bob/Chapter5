e2e59c64ce7e59d44d06310d073a25ea530eeed3
bobsmall <bob.small@blackboard.com>
2018-03-19 16:35:09 -0700

    added timer and times up msg

--- Assets/Scripts/timer.js
+++ Assets/Scripts/timer.js
@@ -2,12 +2,24 @@
 var time:float;
 
 function Start () {
-time = 0.0f;
+	time = 0.0f;
+	GameObject.Find("timerUI").GetComponent.<UI.Text>().text = "";
+	GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text = "";
 }
 
 function Update () {
-time = time + Time.deltaTime;
-var seconds:int = time;
-print("Time:"+seconds);
-
+	time = time + Time.deltaTime;
+	var seconds:int = time;
+	var minutes:int = time/60;
+	seconds = time%60;
+	GameObject.Find("timerUI").GetComponent.<UI.Text>().text = minutes+":"+seconds;
+	if(time>118)
+	{
+	GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text = "Time Almost Up";
+	}
+	if(time>120)
+		{
+		print("TIME UP, restart.");
+		Application.LoadLevel("indoor");
+		}
 }
