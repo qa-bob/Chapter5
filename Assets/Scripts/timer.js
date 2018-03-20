bbe589b0f59d64f13a8ce7bde34638833d2ea1f3
bobsmall <bob.small@blackboard.com>
2018-03-20 14:46:26 -0700

    added Fonts, ASU, and Cube Objects

--- Assets/Scripts/timer.js
+++ Assets/Scripts/timer.js
@@ -2,7 +2,7 @@
 var time:float;
 
 function Start () {
-	time = 0.0f;
+	time = 110.0f;
 	GameObject.Find("timerUI").GetComponent.<UI.Text>().text = "";
 	GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text = "";
 }
@@ -13,13 +13,31 @@ function Update () {
 	var minutes:int = time/60;
 	seconds = time%60;
 	GameObject.Find("timerUI").GetComponent.<UI.Text>().text = minutes+":"+seconds;
-	if(time>118)
+	if(time>115)
 	{
-	GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text = "Time Almost Up";
+	GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text = "Time is Almost Up";
+	if(time >116)
+		{
+		GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text ="";
+		}
 	}
 	if(time>120)
 		{
-		print("TIME UP, restart.");
-		Application.LoadLevel("indoor");
+		GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text = "TIME IS UP, RESTARTING...";
+//		sleep(1000);
+		if(time>122)
+			{
+			GameObject.Find("userMessageUI").GetComponent.<UI.Text>().text ="";
+			Application.LoadLevel("indoor");
+			time = 110.0f;
+			}
 		}
+	
 }
+
+//function sleep(ms:long) {
+//                var currentTime = new Date().Now;
+//                while (currentTime + ms >= new Date().Now) {
+//                print("in 3");
+//                }
+//            }
